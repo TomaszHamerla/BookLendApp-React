c8b380cab25cc2eef0bff907cb118be6d54ab462
unknown <tomaszhamerla@interia.pl>
2024-01-20 17:09:35 +0100

    added useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,24 @@ import './App.css'
 import { Loans } from './components/Loans'
 import { Users } from './components/Users'
 import { Books } from './components/Books'
+import { useState } from 'react'
 
 function App() {
+  const [usersState, setUsersState] = useState(false)
+
+  const handleUsersState = () => {
+    setUsersState(!usersState)
+  }
 
   return (
     <div>
-      <Users />
-      <Books />
-      <Loans />
+      {usersState ? <Users /> : <div>
+        <button onClick={handleUsersState}>show users</button>
+        <Loans />
+        <Books />
+      </div>
+      }
+
     </div>
   )
 }
