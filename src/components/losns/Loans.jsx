5dd9697157bf420300ca2c3f4a7566b932e60478
Tomasz Hamerla <tomaszhamerla@interia.pl>
2024-02-18 12:54:12 +0100

    added connection with loans api

--- src/components/losns/Loans.jsx
+++ src/components/losns/Loans.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Loans.css";
 
 export const Loans = () => {
+  const [list, setList] = useState([]);
+  const getLoans = async () => {
+    const response = await fetch(
+      "http://localhost:8080/loans/searchByLoanStatus?status=true",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const loans = await response.json();
+    setList(loans);
+  };
+
+  useEffect(() => {
+    getLoans();
+  }, []);
   return (
     <div>
       <div className="header">
@@ -16,68 +34,17 @@ export const Loans = () => {
             <h2>Books</h2>
           </div>
         </div>
-        <div className="dataTable">
-          <div className="title">Tomasz</div>
-          <div className="books">
-            <div className="data">
-              <div>Harry Potter 1</div>
-              <button>Return book</button>
+        {list.map((l) => (
+          <div key={l.loanId} className="dataTable">
+            <div className="title">{l.lastName}</div>
+            <div className="books">
+              <div className="data">
+                <div>{l.book.title}</div>
+                <button>Return book</button>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="dataTable">
-          <div className="title">Tomasz</div>
-          <div className="books">
-            <div className="data">
-              <div>Harry Potter 1</div>
-              <button>Return book</button>
-            </div>
-            <div className="data">
-              <div>Harry Potter 1</div>
-              <button>Return book</button>
-            </div>
-            <div className="data">
-              <div>Harry Potter 1</div>
-              <button>Return book</button>
-            </div>
-          </div>
-        </div>
-        <div className="dataTable">
-          <div className="title">Tomasz</div>
-          <div className="books">
-            <div className="data">
-              <div>Harry Potter 1</div>
-              <button>Return book</button>
-            </div>
-          </div>
-        </div>
-        <div className="dataTable">
-          <div className="title">Tomasz</div>
-          <div className="books">
-            <div className="data">
-              <div>Harry Potter 1</div>
-              <button>Return book</button>
-            </div>
-          </div>
-        </div>
-        <div className="dataTable">
-          <div className="title">Tomasz</div>
-          <div className="books">
-            <div className="data">
-              <div>Harry Potter 1</div>
-              <button>Return book</button>
-            </div>
-          </div>
-        </div>
-        <div className="dataTable">
-          <div className="title">Tomasz</div>
-          <div className="books">
-            <div className="data">
-              <div>Harry Potter 1</div>
-              <button>Return book</button>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
       <div className="inputDiv">
         <input
